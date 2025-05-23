86c76367c0ee9b8784d868bf86baa5c8f7624a68
Waruna7 <kavinduhiroshan17@gmail.com>
2025-05-23 21:15:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined? "ENTER YOUR SESSION ID":process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined? "hnUjGTQD#fNGvT8X0V9yr4xJCHB48c4Ygo9D8HNU6C8oSTFLC_gU":process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "mongodb://mongo:LGSeWGEGWTtBGgQoNqIIdmMfGCEqAOrZ@ballast.proxy.rlwy.net:45327",
 GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyDzyQ7tnQkquWX8bc1y9TE0auCHbC_QQO8",
 tokenm: process.env.GITHUB_TOKEN || "false",
